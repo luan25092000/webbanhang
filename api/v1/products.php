e00fbccc790e21b42324c3e7f1145d6c13ca6360
NGO SANG <46277436+ngosangns@users.noreply.github.com>
2021-02-07 23:41:55 +0700

    Update ProductAPI

--- api/v1/products.php
+++ api/v1/products.php
@@ -4,56 +4,58 @@ require_once($_SERVER["DOCUMENT_ROOT"]."/libs/mysql.php");
 require_once($_SERVER["DOCUMENT_ROOT"]."/models/product.php");
 require_once($_SERVER["DOCUMENT_ROOT"]."/models/response.php");
 
-function gets() {
-    // Connect db
-    $conn = connect_db();
-    // Query
-    $res = mysql_get_data_from_query($conn, "SELECT * from product");
-    return $res;
-}
-
-function get(String $id) {
-    // Connect db
-    $conn = connect_db();
-    // Query
-    $res = mysql_get_data_from_query($conn, "SELECT * from product WHERE id=".$id."");
-    return $res;
-}
-
-function post(Product $product) {
-    // Connect db
-    $conn = connect_db();
-    // Query
-    $query = `
-        INSERT INTO product(\`title\`, \`price\`, \`quantity\`, \`createdAt\`, \`updatedAt\`)
-            VALUES(\``.$product->title.`\`, \``.$product->price.`\`, \``.$product->quantity.`\`, \``.date("Y-m-d H:i:s").`\`, \``.date("Y-m-d H:i:s").`\`)
-    `;
-    $res = mysql_post_data_from_query($conn, $query);
-    print $res;
-}
-
-function update(Product $product) {
-    // Connect db
-    $conn = connect_db();
-    // Query
-    $query = `
-        UPDATE product
-            SET \`title\`=\``.$product->title.`\`, \`price\`=\``.$product->price.`\`, \`quantity\`=\``.$product->quantity.`\`, \`updatedAt\`=\``.date("Y-m-d H:i:s").`\`
-            WHERE \`id\`=\``.$product->id.`\`
-    `;
-    $res = mysql_post_data_from_query($conn, $query);
-    print $res;
-}
-
-function delete(String $id) {
-    // Connect db
-    $conn = connect_db();
-    // Query
-    $query = `
-        DELETE FROM product WHERE \`id\`=\``.$id.`\`
-    `;
-    $res = mysql_post_data_from_query($conn, $query);
-    print $res;
+class ProductAPI {
+    public static function gets() {
+        // Connect db
+        $conn = connect_db();
+        // Query
+        $res = mysql_get_data_from_query($conn, "SELECT * from product");
+        return $res;
+    }
+    
+    public static function get(String $id) {
+        // Connect db
+        $conn = connect_db();
+        // Query
+        $res = mysql_get_data_from_query($conn, "SELECT * from product WHERE id=".$id."");
+        return $res;
+    }
+    
+    public static function post(Product $product) {
+        // Connect db
+        $conn = connect_db();
+        // Query
+        $query = `
+            INSERT INTO product(\`title\`, \`price\`, \`quantity\`, \`createdAt\`, \`updatedAt\`)
+                VALUES(\``.$product->title.`\`, \``.$product->price.`\`, \``.$product->quantity.`\`, \``.date("Y-m-d H:i:s").`\`, \``.date("Y-m-d H:i:s").`\`)
+        `;
+        $res = mysql_post_data_from_query($conn, $query);
+        print $res;
+    }
+    
+    public static function update(Product $product) {
+        // Connect db
+        $conn = connect_db();
+        // Query
+        $query = `
+            UPDATE product
+                SET \`title\`=\``.$product->title.`\`, \`price\`=\``.$product->price.`\`, \`quantity\`=\``.$product->quantity.`\`, \`updatedAt\`=\``.date("Y-m-d H:i:s").`\`
+                WHERE \`id\`=\``.$product->id.`\`
+        `;
+        $res = mysql_post_data_from_query($conn, $query);
+        print $res;
+    }
+    
+    public static function delete(String $id) {
+        // Connect db
+        $conn = connect_db();
+        // Query
+        $query = `
+            DELETE FROM product WHERE \`id\`=\``.$id.`\`
+        `;
+        $res = mysql_post_data_from_query($conn, $query);
+        print $res;
+    }
 }
 
 if(empty($_GET) && empty($_POST)) {
@@ -63,7 +65,7 @@ if(empty($_GET) && empty($_POST)) {
 
 if(isset($_GET["getall"])) {
     error_reporting(E_ERROR | E_PARSE); // Hide warning
-    print gets()->to_json();
+    print ProductAPI::gets()->to_json();
     die();
 }
 ?>
