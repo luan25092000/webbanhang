f550f99da737d92b01f324b01940acd2e0195fc5
KaitoRyouga <kaito1477800@gmail.com>
2021-03-21 12:05:20 +0700

    fix send mail

--- vms/admin/CustomerPage.php
+++ vms/admin/CustomerPage.php
@@ -1,60 +1,88 @@
 <?php
+
 namespace vms\admin;
+
 use api\v1\AccountAPI;
 use vms\templates\ContainerAdminTemplate;
 
-class CustomerPage {
+class CustomerPage
+{
 
-	private $rows;
+  private $rows;
 
-    public function __construct($param = null) {
-		$this->rows = AccountAPI::gets();
-    }
+  public function __construct($param = null)
+  {
+    $this->rows = AccountAPI::gets();
+  }
 
-    // Khai báo template và truyền bản thân vào template cha
-    public function render() {
-        $template = new ContainerAdminTemplate();
-        $template->renderChild($this);
-    }
+  // Khai báo template và truyền bản thân vào template cha
+  public function render()
+  {
+    $template = new ContainerAdminTemplate();
+    $template->renderChild($this);
+  }
 
-    public function __render() {
+  public function __render()
+  {
 
 
-        
-    ?>
 
-<div class="row">
-      	<div class="col-10">
-      		<h2>Customers</h2>
-      	</div>
+?>
+
+    <div class="row">
+      <div class="col-10">
+        <h2>Customers</h2>
       </div>
-      
-      <div class="table-responsive">
-        <table class="table table-striped table-sm">
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>Name</th>
-              <th>Admin</th>
-              <th>Email</th>
-            </tr>
-          </thead>
-          <tbody id="customer_list">
+    </div>
+
+    <div class="table-responsive">
+      <table class="table table-striped table-sm">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Name</th>
+            <th>Admin</th>
+            <th>Email</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody id="customer_list">
           <?php foreach ($this->rows->message as $row) : ?>
             <tr>
               <th><?= $row['id'] ?></th>
               <th><?= $row['username'] ?></th>
               <th><?= $row["admin"] ?></th>
               <th><?= $row['email'] ?></th>
+              <th><button data-toggle="modal" data-target="#show_modal">show modal</button></th>
             </tr>
           <?php endforeach; ?>
-          </tbody>
-        </table>
-      </div>
+        </tbody>
+      </table>
+    </div>
     </main>
-  </div>
-</div>
+    </div>
+    </div>
 
-<!-- <script type="text/javascript" src="assets/js/admin/customers.js"></script> -->
+    <div class="modal fade" id="show_modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Add Product</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <div class="name-pd">
+                <h4 class="title">Ten san pham:</h4>
+                <p class="name-sp">aanh yeu em</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
 
-<?php }}
+    <!-- <script type="text/javascript" src="assets/js/admin/customers.js"></script> -->
+
+<?php }
+}
