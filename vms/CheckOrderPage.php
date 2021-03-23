7b75437ae5f423155a2f1fa6b9d46d67afcecd97
NGO SANG <ngosangns@gmail.com>
2021-03-23 18:15:05 +0700

    chore: Fix CheckOrderPage

--- vms/CheckOrderPage.php
+++ vms/CheckOrderPage.php
@@ -7,7 +7,7 @@ use api\v1\AccountAPI;
 class CheckOrderPage {
     public $title;
     private $account;
-    private $orders;
+    private $orders = [];
 
     public function __construct($params = null) {
         // Set title
@@ -16,14 +16,18 @@ class CheckOrderPage {
 
     // Khai báo template và truyền bản thân vào template cha
     public function render() {
-        // Get the orders if logged
+        // Check auth
         $res = AccountAPI::checkAuthRequest();
+        if(!$res->status) {
+             header("Location: /");
+             return;
+        }
+        $this->account = $res->message;
+
+        // Get order
+        $res = CheckoutAPI::reads($this->account["id"]);
         if($res->status) {
-            $this->account = $res->message;
-            $res = CheckoutAPI::reads($this->account["id"]);
-            if($res->status) {
-                $this->orders = $res->message;
-            }
+            $this->orders = $res->message;
         }
 
         $template = new ContainerTemplate();
@@ -36,26 +40,26 @@ class CheckOrderPage {
 <div class="row check-order-container mt-4">
     <div class="col-lg-10">
         <small><a href="/" class="text-dark">Trang chủ</a> <i class="fas fa-angle-double-right"></i> <span
-                class="introduce">Kiểm tra đơn
-                hàng</span></small>
-
-        <?php if($this->orders) { ?>
-            <div class="heading-lg mt-3 mb-3">
-                <h1>ĐƠN HÀNG CỦA BẠN</h1>
-            </div>
-            <ul class="list-group mb-5">
-                <?php foreach($this->orders as $item) { ?>
-                    <li class="list-group-item d-flex justify-content-between align-items-center">
-                        <a href="/order-detail/<?= $item["code"] ?>"><?= $item["code"] ?></a>
-                        <?php if(!$item["status"]) { ?>
-                            <span class="badge badge-danger badge-pill">
-                                Chưa thanh toán
-                            </span>
-                        <?php } ?>
-                    </li>
-                <?php } ?>
-            </ul>
-        <?php } ?>
+            class="introduce">Kiểm tra đơn hàng</span></small>
+        <div class="heading-lg mt-3 mb-3">
+            <h1>ĐƠN HÀNG CỦA BẠN</h1>
+        </div>
+        <ul class="list-group mb-5">
+            <?php foreach($this->orders as $item) { ?>
+                <li class="list-group-item d-flex justify-content-between align-items-center">
+                    <a href="/order-detail/<?= $item["code"] ?>"><?= $item["code"] ?></a>
+                    <?php if(!$item["status"]) { ?>
+                        <span class="badge badge-danger badge-pill">
+                            Chưa thanh toán
+                        </span>
+                    <?php } ?>
+                </li>
+            <?php } if(!count($this->orders)) { ?>
+                <div class="alert alert-primary" role="alert">
+                    Bạn chưa có đơn hàng nào
+                </div>
+            <?php } ?>
+        </ul>
     </div>
 </div>
 <?php }}
